f60e57629a6c3367cec51d324b7238cef1b3c8d8
kimsiele <sielekim.emmanuel@gmail.com>
2023-11-24 13:09:24 +0300

    Constants refactor

--- app/src/main/java/com/sielehub/treasuremart/core/Constants.kt
+++ app/src/main/java/com/sielehub/treasuremart/core/Constants.kt
@@ -1,4 +1,16 @@
 package com.sielehub.treasuremart.core
 
 class Constants {
+    object HttpRoutes {
+        //private const val BASE_URL ="https://api.escuelajs.co/api/v1" without cart
+        private const val BASE_URL ="https://fakestoreapi.com"
+        const val PRODUCTS_ENDPOINT = "$BASE_URL/products"
+        const val PRODUCT_ENDPOINT = "$BASE_URL/products/"
+        const val CATEGORY_PRODUCTS_ENDPOINT = "$BASE_URL/products/category/"
+        const val CATEGORIES_ENDPOINT = "$BASE_URL/categories"
+        const val CARTS_ENDPOINT = "$BASE_URL/carts"
+        const val USERS_ENDPOINT = "$BASE_URL/users"
+        const val CREATE_USER_ENDPOINT = "$BASE_URL/users/"
+        const val AUTH_ENDPOINT = "$BASE_URL/auth/login/"
+    }
 }
