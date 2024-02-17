251532b5cee94a6a0a231381a2618fdd1f26d158
Siele <sielekim.emmanuel@gmail.com>
2024-02-17 19:11:38 +0300

    User ViewModel implementation

--- app/src/main/java/com/sielehub/treasuremart/presentation/user/UserViewModel.kt
+++ app/src/main/java/com/sielehub/treasuremart/presentation/user/UserViewModel.kt
@@ -4,10 +4,14 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sielehub.treasuremart.core.Resource
 import com.sielehub.treasuremart.domain.model.User
 import com.sielehub.treasuremart.domain.use_case.user.CreateUserUseCase
 import com.sielehub.treasuremart.domain.use_case.user.GetUserUseCase
 import com.sielehub.treasuremart.domain.use_case.user.UpdateUserUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class UserViewModel(
@@ -16,27 +20,70 @@ class UserViewModel(
     private val updateUserUseCase: UpdateUserUseCase
 ) : ViewModel() {
 
-    private val _user = mutableStateOf(null)
-    val user: State<User?> = _user
+    private val _userState = mutableStateOf(UserState())
+    val userState: State<UserState> = _userState
 
-    private val _users = mutableStateOf<List<User>>(emptyList())
-    val users: State<List<User>> = _users
+    private val _createUserState = mutableStateOf(CreateUserState())
+    val createUserState: State<CreateUserState> = _createUserState
+
+    private val _userUpdateState = mutableStateOf(UserUpdateState())
+    val userUpdateState: State<UserUpdateState> = _userUpdateState
 
     fun createUser(user: User) {
         viewModelScope.launch {
-            createUserUseCase.invoke(user)
+            when (val result = createUserUseCase(user)) {
+                is Resource.Success -> {
+                    _createUserState.value = CreateUserState(user = result.data)
+                }
+
+                is Resource.Error -> {
+                    _createUserState.value =
+                        CreateUserState(error = result.message ?: "Unknown error occurred")
+                }
+
+                is Resource.Loading -> {
+                    _createUserState.value = CreateUserState(isLoading = true)
+                }
+            }
         }
     }
 
     fun getUser(userId: Int) {
-        viewModelScope.launch {
-            getUserUseCase.invoke(userId)
-        }
+        getUserUseCase.invoke(userId).onEach {
+            when (val result = it) {
+                is Resource.Success -> {
+                    _userState.value = UserState(user = result.data)
+                }
+
+                is Resource.Error -> {
+                    _userState.value = UserState(error = result.message ?: "Unknown error occurred")
+                }
+
+                is Resource.Loading -> {
+                    _userState.value = UserState(isLoading = true)
+                }
+            }
+
+        }.launchIn(viewModelScope)
+
     }
 
     fun updateUser(user: User) {
-        viewModelScope.launch {
-            updateUserUseCase.invoke(user)
+        viewModelScope.launch(Dispatchers.IO) {
+            when (val result = updateUserUseCase(user)) {
+                is Resource.Success -> {
+                    _userUpdateState.value = UserUpdateState(user = result.data)
+                }
+
+                is Resource.Error -> {
+                    _userUpdateState.value =
+                        UserUpdateState(error = result.message ?: "Unknown error occurred")
+                }
+
+                is Resource.Loading -> {
+                    _userUpdateState.value = UserUpdateState(isLoading = true)
+                }
+            }
         }
     }
 
