9bc4b401628f2414bccb2edbedbab47d178d7b31
Siele <sielekim.emmanuel@gmail.com>
2024-06-04 12:52:33 +0300

    Chore: Add mockk test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.0")*/
 
     testImplementation("junit:junit:4.13.2")
+    implementation("io.mockk:mockk:1.9.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
