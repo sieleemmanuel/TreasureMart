6c8e7ca53e1396febaa22e296e2e0b1d8f7155d4
Siele <sielekim.emmanuel@gmail.com>
2024-06-04 13:31:49 +0300

    Chore: Add coil image loading dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,9 @@ dependencies {
     implementation("io.insert-koin:koin-annotations:1.3.0")
     ksp("io.insert-koin:koin-ksp-compiler:1.3.0")
 
+    // coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     // room
     /*implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
