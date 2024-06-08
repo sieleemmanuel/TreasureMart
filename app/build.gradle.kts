b307b891d82f7ae105cb86dfd29fb50641f102ea
Siele <sielekim.emmanuel@gmail.com>
2024-06-08 08:56:24 +0300

    Chore: Add compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
 
     // ktor
     implementation(libs.ktor.client.core)
