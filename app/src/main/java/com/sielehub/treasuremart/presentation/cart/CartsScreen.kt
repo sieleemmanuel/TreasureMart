680deeb0497fbde264d57aad28f5c1127fa95871
Siele <sielekim.emmanuel@gmail.com>
2024-12-25 10:39:53 +0300

    Feat: Cart UI refactor

--- app/src/main/java/com/sielehub/treasuremart/presentation/cart/CartsScreen.kt
+++ app/src/main/java/com/sielehub/treasuremart/presentation/cart/CartsScreen.kt
@@ -11,17 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -30,30 +30,40 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.sielehub.treasuremart.core.Constants.Companion.myCart
+import com.sielehub.treasuremart.presentation.Route
 import com.sielehub.treasuremart.presentation.cart.component.CartCard
-import com.sielehub.treasuremart.presentation.product.ProductsScreen
-import com.sielehub.treasuremart.presentation.product.component.ProductCard
 
 @Composable
-fun CartsScreen(modifier: Modifier = Modifier) {
-    val itemsCount = rememberSaveable {
-        9
-    }
-    Box(modifier = modifier.fillMaxSize()) {
+fun CartsScreen(
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues,
+    navController: NavHostController
+) {
+    val cart = myCart()
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(bottom = paddingValues.calculateBottomPadding())
+    ) {
         Column(
-            modifier = modifier.fillMaxSize()
+            modifier = modifier
+                .fillMaxSize()
+                .statusBarsPadding()
         ) {
             Row(
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
                     fontWeight = FontWeight.Bold,
                     fontSize = 20.sp,
-                    text = "Cart($itemsCount)"
+                    text = "Cart(${cart.products.size})"
                 )
                 IconButton(onClick = { /*TODO*/ }) {
                     Icon(imageVector = Icons.Default.MoreVert, contentDescription = null)
@@ -64,38 +74,44 @@ fun CartsScreen(modifier: Modifier = Modifier) {
                 modifier = modifier.padding(horizontal = 10.dp),
                 verticalArrangement = Arrangement.spacedBy(10.dp),
             ) {
-                items(itemsCount) {
-                    CartCard()
+                items(items = cart.products) {
+                    CartCard(cartProduct = it)
                 }
-                item{ Spacer(modifier = modifier.height(82.dp))}
+                item { Spacer(modifier = modifier.height(82.dp)) }
             }
         }
         Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .background(color = MaterialTheme.colorScheme.background)
-                    .padding(16.dp)
-                    .align(Alignment.BottomCenter),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-        Text(
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            text = "KSh 156, 000"
-        )
-        ElevatedButton(
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.elevatedButtonColors(containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = MaterialTheme.colorScheme.onPrimary)) {
-            Text(text = "Check Out($itemsCount)")
+            val total = cart.products.map {}
+            Text(
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                text = "KSh 156, 000"
+            )
+            ElevatedButton(
+                onClick = {
+                    navController.navigate(Route.Checkout)
+                },
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            ) {
+                Text(text = "Check Out(${cart.products.size})")
+            }
         }
-    }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CartsScreenPreview() {
-    CartsScreen()
+    CartsScreen(paddingValues = PaddingValues(0.dp), navController = rememberNavController())
 }
--- app/src/main/java/com/sielehub/treasuremart/presentation/cart/component/CartCard.kt
+++ app/src/main/java/com/sielehub/treasuremart/presentation/cart/component/CartCard.kt
@@ -2,10 +2,13 @@ package com.sielehub.treasuremart.presentation.cart.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
@@ -26,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -35,111 +40,138 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.sielehub.treasuremart.R
+import com.sielehub.treasuremart.core.Constants.Companion.products
+import com.sielehub.treasuremart.domain.model.CartProduct
 
 @Composable
-fun CartCard(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(
-                color = MaterialTheme.colorScheme.tertiaryContainer,
-                shape = RoundedCornerShape(16.dp)
-            )
-            .padding(12.dp)
-
-    ) {
-        AsyncImage(
-            model = R.drawable.ic_launcher_background,
-            placeholder = painterResource(id = R.drawable.ic_shopping),
-            contentDescription = null,
-            modifier = modifier
-                .clip(RoundedCornerShape(16.dp))
-                .size(80.dp)
-        )
-        Column(
+fun CartCard(
+    modifier: Modifier = Modifier,
+    cartProduct: CartProduct,
+    onClick: (Int) -> Unit = {},
+    onRemove: (Int) -> Unit = {},
+) {
+    val product = products().find { it.id == cartProduct.productId }
+    ElevatedCard(onClick = { /*TODO*/ }) {
+        Row(
             modifier = modifier
                 .fillMaxWidth()
-                .padding(start = 12.dp)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+
         ) {
-            Row(modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,) {
-                Text(
-                    text = "Product name here",
-                    fontWeight = FontWeight.Bold
+            Box(modifier = modifier
+                .size(90.dp)
+                .background(color = MaterialTheme.colorScheme.surfaceVariant, shape = RoundedCornerShape(8.dp))
+                .clip(RoundedCornerShape(8.dp))
+            ) {
+                AsyncImage(
+                    model = product?.image,
+                    placeholder = painterResource(id = R.drawable.ic_shopping),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(16.dp))
+                        .clickable {
+                            product?.id?.let { onClick(it) }
+                        }
                 )
-                IconButton(
-                    onClick = { /*TODO*/ },
-                    colors = IconButtonDefaults.outlinedIconButtonColors(
-                        contentColor = Color.Gray
-                    ),
-                    modifier = modifier.size(24.dp)) {
-                    Icon(
-                        imageVector = Icons.Outlined.Clear,
-                        contentDescription = null,
-                        )
-                }
             }
-            Text(
-                text = "Brief description here",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    color = Color.Gray
-                ),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Spacer(modifier = modifier.height(16.dp))
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
+
+            Column(
                 modifier = modifier
                     .fillMaxWidth()
+                    .padding(start = 12.dp)
             ) {
-                Text(
-                    style = TextStyle(color = MaterialTheme.colorScheme.primary),
-                    text = "KSh 18, 000",
-                    fontWeight = FontWeight.Bold
-                )
-
                 Row(
-                    modifier = modifier
-                        .padding(start = 12.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
                 ) {
-                    OutlinedIconButton(
-                        modifier = modifier.size(28.dp),
+                    Text(
+                        text = product?.title?:"",
+                        fontWeight = FontWeight.Bold,
+                        minLines = 2,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = modifier.weight(.9f)
+                    )
+                    IconButton(
+                        onClick = { product?.id?.let { onRemove(it) } },
                         colors = IconButtonDefaults.outlinedIconButtonColors(
                             contentColor = Color.Gray
                         ),
-                        border = BorderStroke(color = Color.Gray, width = 1.dp),
-                        shape = RoundedCornerShape(8.dp),
-                        onClick = {
-
-                        }) {
-                        Icon(imageVector = Icons.Default.Remove, contentDescription = null)
+                        modifier = modifier.size(24.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Clear,
+                            contentDescription = null,
+                        )
                     }
-                    Text(text = "2", style = TextStyle(fontWeight = FontWeight.Bold))
-                    OutlinedIconButton(
-                        modifier = modifier.size(28.dp),
-                        colors = IconButtonDefaults.outlinedIconButtonColors(
-                            contentColor = MaterialTheme.colorScheme.primary
-                        ),
-                        border = BorderStroke(color = MaterialTheme.colorScheme.primary, width = 1.dp),
-                        shape = RoundedCornerShape(8.dp),
-                        onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+                Text(
+                    text = product?.description?:"",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = modifier.height(16.dp))
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        style = TextStyle(color = MaterialTheme.colorScheme.primary),
+                        text = "KSh ${(product?.price?.times(cartProduct.quantity)?.times(130) ?: 1)}",
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Row(
+                        modifier = modifier
+                            .padding(start = 12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        OutlinedIconButton(
+                            modifier = modifier.size(24.dp),
+                            colors = IconButtonDefaults.outlinedIconButtonColors(
+                                contentColor = Color.Gray
+                            ),
+                            border = BorderStroke(color = Color.Gray, width = 1.dp),
+                            shape = RoundedCornerShape(4.dp),
+                            onClick = {
+
+                            }) {
+                            Icon(imageVector = Icons.Default.Remove, contentDescription = null)
+                        }
+                        Text(text = cartProduct.quantity.toString(), style = TextStyle(fontWeight = FontWeight.Bold))
+                        OutlinedIconButton(
+                            modifier = modifier.size(24.dp),
+                            colors = IconButtonDefaults.outlinedIconButtonColors(
+                                contentColor = MaterialTheme.colorScheme.primary
+                            ),
+                            border = BorderStroke(
+                                color = MaterialTheme.colorScheme.primary,
+                                width = 1.dp
+                            ),
+                            shape = RoundedCornerShape(4.dp),
+                            onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                        }
                     }
                 }
             }
+
         }
-
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CartCardPreview() {
-    CartCard()
+    CartCard(cartProduct = CartProduct(1, 1))
 }
