9fe4b4d1a6910398459771ba1a92abb796e8d613
Siele <sielekim.emmanuel@gmail.com>
2025-01-21 21:46:57 +0300

    Add Datastore manager logic

---
+++ app/src/test/java/com/sielehub/treasuremart/data/datastore/DataStoreManagerTest.kt
@@ -0,0 +1,13 @@
+package com.sielehub.treasuremart.data.datastore
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+
+private val TEST_DATASTORE_NAME = "setting_test"
+
+@RunWith(AndroidJUnit4::class)
+class DataStoreManagerTest {
+    
+
+}
