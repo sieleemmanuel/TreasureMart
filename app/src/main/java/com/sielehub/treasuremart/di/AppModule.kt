f7b0627deec9f11d8798f73f9c06f5e70dfda122
Siele <sielekim.emmanuel@gmail.com>
2025-04-07 22:11:04 +0300

    feat: Add Preferences DataStore

--- app/src/main/java/com/sielehub/treasuremart/di/AppModule.kt
+++ app/src/main/java/com/sielehub/treasuremart/di/AppModule.kt
@@ -1,9 +1,16 @@
 package com.sielehub.treasuremart.di
 
-import android.os.Build
-import androidx.annotation.RequiresExtension
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.sielehub.treasuremart.data.datastore.DataStoreManager
+import com.sielehub.treasuremart.data.remote.StoreService
 import com.sielehub.treasuremart.data.remote.StoreServiceImp
+import com.sielehub.treasuremart.data.repository.AuthRepositoryImpl
 import com.sielehub.treasuremart.data.repository.StoreRepositoryImpl
+import com.sielehub.treasuremart.domain.repository.AuthRepository
+import com.sielehub.treasuremart.domain.repository.StoreRepository
 import com.sielehub.treasuremart.domain.use_case.cart.CreateCartUseCase
 import com.sielehub.treasuremart.domain.use_case.cart.GetCartUseCase
 import com.sielehub.treasuremart.domain.use_case.cart.GetCartsUseCase
@@ -14,22 +21,28 @@ import com.sielehub.treasuremart.domain.use_case.product.GetProductsByCategoryUs
 import com.sielehub.treasuremart.domain.use_case.product.GetProductsUseCase
 import com.sielehub.treasuremart.domain.use_case.user.CreateUserUseCase
 import com.sielehub.treasuremart.domain.use_case.user.GetUserUseCase
+import com.sielehub.treasuremart.domain.use_case.user.LoginUseCase
 import com.sielehub.treasuremart.domain.use_case.user.UpdateUserUseCase
+import com.sielehub.treasuremart.presentation.auth.AuthViewModel
 import com.sielehub.treasuremart.presentation.cart.CartViewModel
 import com.sielehub.treasuremart.presentation.categories.CategoriesViewModel
+import com.sielehub.treasuremart.presentation.onboarding.OnBoardingViewModel
 import com.sielehub.treasuremart.presentation.product.ProductViewModel
-import com.sielehub.treasuremart.presentation.user.UserViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 object AppModule {
-    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
     val appModule = module {
         single {
             HttpClient(Android) {
@@ -41,9 +54,12 @@ object AppModule {
                 }
             }
         }
+        single<DataStore<Preferences>> { androidContext().dataStore }
+        factory { DataStoreManager(get()) }
 
-        factory { StoreServiceImp(get()) }
-        factory { StoreRepositoryImpl(get()) }
+        singleOf(::StoreServiceImp) { bind<StoreService>() }
+        singleOf(::AuthRepositoryImpl) { bind<AuthRepository>() }
+        singleOf(::StoreRepositoryImpl) { bind<StoreRepository>() }
 
         factory { GetProductsUseCase(get()) }
         factory { GetProductUseCase(get()) }
@@ -59,8 +75,10 @@ object AppModule {
         factory { GetUserUseCase(get()) }
         factory { CreateUserUseCase(get()) }
         factory { UpdateUserUseCase(get()) }
+        factory { LoginUseCase(get()) }
 
-        factory { UserViewModel(get(), get(), get()) }
+        viewModelOf(::OnBoardingViewModel)
+        viewModelOf(::AuthViewModel)
         viewModelOf(::ProductViewModel)
         viewModelOf(::CartViewModel)
         viewModelOf(::CategoriesViewModel)
