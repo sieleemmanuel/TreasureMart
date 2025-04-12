3d99e9498c98a4b2f75d2faf0bc7bfc9702e9fb0
Siele <sielekim.emmanuel@gmail.com>
2025-04-12 22:09:02 +0300

    Refactor: Move GetUserUseCase to account package and update logic

---
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/account/GetUserUseCase.kt
@@ -0,0 +1,20 @@
+package com.sielehub.treasuremart.domain.use_case.account
+
+import com.sielehub.treasuremart.core.Resource
+import com.sielehub.treasuremart.domain.model.User
+import com.sielehub.treasuremart.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetUserUseCase(private val authRepository: AuthRepository) {
+
+    operator fun invoke(userId: Int): Flow<Resource<User?>> = flow {
+        try {
+            emit(Resource.Loading())
+            val user = authRepository.getUser(userId)
+            emit(Resource.Success(data = user))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "An unknown error occurred"))
+        }
+    }
+}
