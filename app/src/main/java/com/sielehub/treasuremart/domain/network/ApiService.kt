fa0e3bb6def72a7b9b60644739d03f891f04e0f9
Siele <sielekim.emmanuel@gmail.com>
2025-04-15 22:12:43 +0300

    Add ApiService interface with product, user, and cart related methods.

---
+++ app/src/main/java/com/sielehub/treasuremart/domain/network/ApiService.kt
@@ -0,0 +1,38 @@
+package com.sielehub.treasuremart.domain.network
+
+import com.sielehub.treasuremart.data.remote.dto.CartDto
+import com.sielehub.treasuremart.data.remote.dto.ProductDto
+import com.sielehub.treasuremart.data.remote.dto.UserDto
+import com.sielehub.treasuremart.domain.model.Cart
+import com.sielehub.treasuremart.domain.model.SignupRequest
+import com.sielehub.treasuremart.domain.model.Token
+import com.sielehub.treasuremart.domain.model.User
+
+interface ApiService {
+
+    suspend fun getProducts(): List<ProductDto>
+
+    suspend fun getProduct(id: Int): ProductDto?
+
+    suspend fun getProductsByCategory(category: String): List<ProductDto>
+
+    suspend fun sortProducts(isASC: Boolean): List<ProductDto>
+
+    suspend fun getCategories(): List<String>
+
+    suspend fun getUser(userId: Int): UserDto?
+
+    suspend fun createUser(signupRequest: SignupRequest): SignupRequest?
+
+    suspend fun updateUser(user: User): UserDto?
+
+    suspend fun authenticateUser(username: String, password: String): Token?
+
+    suspend fun getCarts(): List<CartDto>
+
+    suspend fun getCart(id: Int): CartDto?
+
+    suspend fun createCart(newCart: Cart): CartDto?
+
+    suspend fun updateCart(cart: Cart): CartDto?
+}
