b28633368bac05c7d0571f7403660cb1746b8c98
Siele <sielekim.emmanuel@gmail.com>
2025-04-15 22:12:16 +0300

    Add Bottom Navigation item data class

---
+++ app/src/main/java/com/sielehub/treasuremart/presentation/dashboard/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.sielehub.treasuremart.presentation.dashboard
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val label: String,
+    val icon: ImageVector,
+    val route: Any
+)
