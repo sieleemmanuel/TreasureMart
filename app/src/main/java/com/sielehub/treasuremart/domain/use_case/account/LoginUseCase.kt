b1e34c43f14e27fd14337d8532954d499299959b
Siele <sielekim.emmanuel@gmail.com>
2025-04-17 21:45:04 +0300

    Add Login use case logic

---
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/account/LoginUseCase.kt
@@ -0,0 +1,23 @@
+package com.sielehub.treasuremart.domain.use_case.account
+
+import android.util.Log
+import com.sielehub.treasuremart.core.Resource
+import com.sielehub.treasuremart.domain.model.Token
+import com.sielehub.treasuremart.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class LoginUseCase(private val authRepository: AuthRepository) {
+
+    operator fun invoke(username: String, password: String): Flow<Resource<Token?>> = flow {
+        try {
+            emit(Resource.Loading())
+            emit(Resource.Success(authRepository.authenticateUser(username, password)))
+        } catch (e: Exception) {
+            Log.d(LoginUseCase::class.simpleName, "Login error: ${e.localizedMessage}")
+            emit(
+                Resource.Error(e.localizedMessage ?: "An unknown error occurred")
+            )
+        }
+    }
+}
