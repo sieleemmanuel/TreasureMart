208d54cf9ce66160c5983cf3056273c8b6385ab6
Siele <sielekim.emmanuel@gmail.com>
2025-04-21 14:58:11 +0300

    feat: Add product card grid and list ui

---
+++ app/src/main/java/com/sielehub/treasuremart/presentation/ui/product/component/ProductCardGrid.kt
@@ -0,0 +1,126 @@
+package com.sielehub.treasuremart.presentation.ui.product.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import com.sielehub.treasuremart.R
+import com.sielehub.treasuremart.domain.model.Product
+
+@Composable
+fun ProductCardGrid(
+    modifier: Modifier = Modifier,
+    product: Product,
+    onFavClick: () -> Unit = {},
+    onClick: () -> Unit,
+) {
+    ElevatedCard(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Box(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            AsyncImage(
+                model = product.image,
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                placeholder = rememberAsyncImagePainter(R.drawable.loading_progress),
+                modifier = modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+            )
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.BottomEnd),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .background(color = Color.Black.copy(alpha = 0.4f), shape = CircleShape)
+                        .padding(6.dp)
+                        .size(24.dp)
+                        .align(Alignment.Center)
+                        .clickable {
+                            onFavClick()
+                        }
+                )
+            }
+        }
+
+        Spacer(modifier = modifier.height(8.dp))
+        Text(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(PaddingValues(horizontal = 8.dp)),
+            text = product.title,
+            style = MaterialTheme.typography.labelMedium,
+            minLines = 2,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(modifier = modifier.height(8.dp))
+        Text(
+            modifier = modifier.padding(PaddingValues(horizontal = 8.dp)),
+            text = "KSh ${product.price.times(130)}",
+            style = MaterialTheme.typography.titleSmall,
+        )
+        Spacer(modifier = modifier.height(8.dp))
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(PaddingValues(horizontal = 8.dp)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            StarRatingBar(
+                rating = product.rating.rate.toFloat(),
+                onRatingChanged = {
+                }
+            )
+            Spacer(modifier = modifier.width(4.dp))
+            Text(
+                text = "(${product.rating.count})",
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+        Spacer(modifier = modifier.height(16.dp))
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun ProductCardPreview() {
+    ProductCardGrid(product = Product()) {}
+}
---
+++ app/src/main/java/com/sielehub/treasuremart/presentation/ui/product/component/ProductCardList.kt
@@ -0,0 +1,129 @@
+package com.sielehub.treasuremart.presentation.ui.product.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import com.sielehub.treasuremart.R
+import com.sielehub.treasuremart.domain.model.Product
+
+@Composable
+fun ProductCardList(
+    modifier: Modifier = Modifier,
+    product: Product,
+    onClick: (productId: Int) -> Unit,
+) {
+    ElevatedCard(
+        onClick = { onClick(product.id) },
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Box(
+                modifier = modifier
+                    .size(90.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surfaceVariant,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                AsyncImage(
+                    model = product.image,
+                    placeholder = rememberAsyncImagePainter(R.drawable.loading_progress),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(16.dp))
+                        .clickable {
+                            onClick(product.id)
+                        }
+                )
+            }
+            Spacer(modifier = modifier.width(8.dp))
+            Column {
+                Text(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp),
+                    text = product.title,
+                    style = MaterialTheme.typography.labelMedium,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
+                )
+                Spacer(modifier = modifier.height(8.dp))
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = modifier.padding(PaddingValues(horizontal = 4.dp)),
+                        text = "KSh ${product.price.times(130)}",
+                        style = MaterialTheme.typography.titleSmall,
+                    )
+                    Text(
+                        modifier = modifier.padding(PaddingValues(horizontal = 4.dp)),
+                        text = "x1",
+                        style = MaterialTheme.typography.titleSmall,
+                        color = Color.Gray
+                    )
+                }
+
+            }
+        }
+
+
+        /* if (addButton) {
+             Button(
+                 modifier = modifier
+                     .padding(PaddingValues(horizontal = 4.dp))
+                     .fillMaxWidth()
+                     .heightIn(24.dp),
+                 shape = RoundedCornerShape(4.dp),
+                 onClick = { *//*TODO*//* }
+            ) {
+                Text(text = "Add to cart")
+            }
+            //Spacer(modifier = modifier.height(4.dp))
+        }*/
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun ProductCardListPreview() {
+    ProductCardList(product = Product()) {}
+}
