35a326980bc367405e6db52a92b0ce4ba37f4e99
Siele <sielekim.emmanuel@gmail.com>
2025-04-21 15:27:26 +0300

    Add compose compiler plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.compose.compiler) apply false
 
 }
 
