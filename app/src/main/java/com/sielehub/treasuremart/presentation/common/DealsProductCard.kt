f4bc1a6f44b9c3d0fad75827c670b498a432bb06
Siele <sielekim.emmanuel@gmail.com>
2025-04-21 15:49:10 +0300

    Add DealsProductCard UI composable

---
+++ app/src/main/java/com/sielehub/treasuremart/presentation/common/DealsProductCard.kt
@@ -0,0 +1,73 @@
+package com.sielehub.treasuremart.presentation.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import com.sielehub.treasuremart.R
+import com.sielehub.treasuremart.domain.model.Product
+
+@Composable
+fun DealsProductCard(
+    modifier: Modifier = Modifier,
+    product: Product,
+    onClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .wrapContentWidth()
+            .wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .clickable { onClick() }
+                .background(color = Color.LightGray)
+
+        ) {
+            AsyncImage(
+                model = product.image,
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                placeholder = rememberAsyncImagePainter(R.drawable.loading_progress),
+                modifier = modifier.fillMaxSize()
+            )
+        }
+        Spacer(modifier = modifier.height(4.dp))
+        Text(
+            modifier = modifier.padding(PaddingValues(horizontal = 8.dp)),
+            text = "KSh ${product.price.times(130)}",
+            style = MaterialTheme.typography.titleSmall,
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun ProductCardPreview() {
+    DealsProductCard(product = Product()) {}
+}
