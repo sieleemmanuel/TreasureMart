a537d937c247fb21cfd97c98100e33cb4eb9450e
Siele <sielekim.emmanuel@gmail.com>
2025-04-28 21:59:58 +0300

    Refactor: DataStoreManager logic and add tests

--- app/src/test/java/com/sielehub/treasuremart/data/datastore/DataStoreManagerTest.kt
+++ app/src/test/java/com/sielehub/treasuremart/data/datastore/DataStoreManagerTest.kt
@@ -1,13 +1,56 @@
 package com.sielehub.treasuremart.data.datastore
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.*
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-private val TEST_DATASTORE_NAME = "setting_test"
+private const val TEST_DATASTORE_NAME = "settings_test.preferences_pb"
 
-@RunWith(AndroidJUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(JUnit4::class)
 class DataStoreManagerTest {
-    
+    private lateinit var context: Context
+    private lateinit var dataStoreManager: DataStoreManager
 
+    @get:Rule
+    val tmpFolder: TemporaryFolder = TemporaryFolder.builder().assureDeletion().build()
+    private val tesDispatcher = UnconfinedTestDispatcher()
+    private val testScope = TestScope(tesDispatcher + Job())
+
+
+    private val testDataStore: DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            scope = testScope,
+            produceFile = { tmpFolder.newFile(TEST_DATASTORE_NAME) }
+        )
+
+    @Before
+    fun setUp() {
+        context = mockk()
+        dataStoreManager = DataStoreManager(testDataStore)
+    }
+
+    @Test
+    fun `when setOnBoardingDone is called, onBoardingDone should be updated`() =
+        testScope.runTest {
+            val initialValue = dataStoreManager.onBoardingDone.first()
+            assert(!initialValue)
+            val onBoardingDone = true
+            dataStoreManager.setOnBoardingDone(onBoardingDone)
+            assert(dataStoreManager.onBoardingDone.first() == onBoardingDone)
+        }
 }
