99cd8683bc0f4d87c0d39005f5a3f79fc5676fc9
Siele <sielekim.emmanuel@gmail.com>
2025-04-29 17:07:09 +0300

    Refactor: Product use cases logic

--- app/src/main/java/com/sielehub/treasuremart/domain/use_case/product/GetProductUseCase.kt
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/product/GetProductUseCase.kt
@@ -1,11 +1,19 @@
 package com.sielehub.treasuremart.domain.use_case.product
 
 import com.sielehub.treasuremart.core.Resource
+import com.sielehub.treasuremart.data.repository.StoreRepositoryImpl
 import com.sielehub.treasuremart.domain.model.Product
-import com.sielehub.treasuremart.domain.repository.StoreRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class GetProductUseCase(private val storeRepository: StoreRepository) {
-    operator fun invoke(productId: Int): Flow<Resource<Product?>>  = storeRepository.getProduct(productId)
-
+class GetProductUseCase(private val storeRepositoryImpl: StoreRepositoryImpl) {
+    operator fun invoke(productId: Int): Flow<Resource<Product?>> = flow {
+        try {
+            emit(Resource.Loading())
+            val productResult = storeRepositoryImpl.getProduct(productId)
+            emit(Resource.Success(productResult))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "An unknown error occurred"))
+        }
+    }
 }
--- app/src/main/java/com/sielehub/treasuremart/domain/use_case/product/GetProductsUseCase.kt
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/product/GetProductsUseCase.kt
@@ -1,12 +1,28 @@
 package com.sielehub.treasuremart.domain.use_case.product
 
 import com.sielehub.treasuremart.core.Resource
+import com.sielehub.treasuremart.data.repository.StoreRepositoryImpl
 import com.sielehub.treasuremart.domain.model.Product
-import com.sielehub.treasuremart.domain.repository.StoreRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class GetProductsUseCase(private val storeRepository: StoreRepository) {
+class GetProductsUseCase(private val storeRepositoryImpl: StoreRepositoryImpl) {
 
-    operator fun invoke(): Flow<Resource<List<Product>>> = storeRepository.getProducts()
+    operator fun invoke(productsQuery: String? = null): Flow<Resource<List<Product>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val productsResult = storeRepositoryImpl.getProducts()
+            productsQuery?.let {
+                val filteredProducts = productsResult.filter { product ->
+                    product.category.contains(productsQuery, ignoreCase = true)
+                            || product.title.contains(productsQuery, ignoreCase = true)
+                            || product.description.contains(productsQuery, ignoreCase = true)
+                }
+                emit(Resource.Success(filteredProducts))
+            } ?: emit(Resource.Success(productsResult))
+
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "An unknown error occurred"))
+        }
+    }
 }
