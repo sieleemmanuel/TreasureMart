e240668415171d32991a2808635a4a8719171bcf
Siele <sielekim.emmanuel@gmail.com>
2025-04-29 17:13:51 +0300

    Refactor: UpdateCart use case logic

--- app/src/main/java/com/sielehub/treasuremart/domain/use_case/cart/UpdateCartUseCase.kt
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/cart/UpdateCartUseCase.kt
@@ -1,9 +1,19 @@
 package com.sielehub.treasuremart.domain.use_case.cart
 
+import com.sielehub.treasuremart.core.Resource
 import com.sielehub.treasuremart.domain.model.Cart
 import com.sielehub.treasuremart.domain.repository.StoreRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class UpdateCartUseCase(private val storeRepository: StoreRepository) {
 
-    suspend operator fun invoke(cart: Cart) = storeRepository.updateCart(cart)
+    operator fun invoke(cart: Cart): Flow<Resource<Cart?>> = flow {
+        try {
+            emit(Resource.Loading())
+            emit(Resource.Success(data = storeRepository.updateCart(cart)))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "An unknown error occurred"))
+        }
+    }
 }
