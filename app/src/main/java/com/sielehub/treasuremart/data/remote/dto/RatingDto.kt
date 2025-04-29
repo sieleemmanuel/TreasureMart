e15c8429a49173b5d396de4d90e7c6230a295c72
Siele <sielekim.emmanuel@gmail.com>
2025-04-29 17:11:59 +0300

    Add: Serializable annotation to RatingDto

--- app/src/main/java/com/sielehub/treasuremart/data/remote/dto/RatingDto.kt
+++ app/src/main/java/com/sielehub/treasuremart/data/remote/dto/RatingDto.kt
@@ -1,10 +1,12 @@
 package com.sielehub.treasuremart.data.remote.dto
 
 import com.sielehub.treasuremart.domain.model.Rating
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class RatingDto(
     val count: Int,
     val rate: Double
-){
+) {
     fun toRating() = Rating(count = count, rate = rate)
 }
