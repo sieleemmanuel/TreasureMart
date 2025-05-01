624554900d0475206133b1be353c2faf70cf4cd0
Siele <sielekim.emmanuel@gmail.com>
2025-05-01 21:53:19 +0300

    Add Room database for local data persistence.

---
+++ app/src/main/java/com/sielehub/treasuremart/data/local/database/Converter.kt
@@ -0,0 +1,32 @@
+package com.sielehub.treasuremart.data.local.database
+
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.sielehub.treasuremart.domain.model.CartProduct
+import com.sielehub.treasuremart.domain.model.Rating
+import com.sielehub.treasuremart.presentation.util.jsonToModel
+import com.sielehub.treasuremart.presentation.util.modelToJsonArray
+
+@TypeConverters
+class Converter {
+    @TypeConverter
+    fun ratingToString(rating: Rating): String = rating.modelToJsonArray()
+
+    @TypeConverter
+    fun stringToRating(string: String): Rating = string.jsonToModel()
+
+    @TypeConverter
+    fun cartProductToString(cartProduct: CartProduct): String = cartProduct.modelToJsonArray()
+
+    @TypeConverter
+    fun stringToCartProduct(string: String): CartProduct = string.jsonToModel()
+
+    @TypeConverter
+    fun cartProductsToString(cartProducts: List<CartProduct>): String =
+        cartProducts.modelToJsonArray()
+
+    @TypeConverter
+    fun stringToCartProducts(string: String): List<CartProduct> = string.jsonToModel()
+
+
+}
---
+++ app/src/main/java/com/sielehub/treasuremart/data/local/database/StoreDb.kt
@@ -0,0 +1,37 @@
+package com.sielehub.treasuremart.data.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.sielehub.treasuremart.domain.model.Cart
+import com.sielehub.treasuremart.domain.model.Product
+import com.sielehub.treasuremart.domain.model.WishProduct
+
+@Database(
+    entities = [
+        Product::class,
+        WishProduct::class,
+        Cart::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(Converter::class)
+abstract class StoreDb : RoomDatabase() {
+    abstract val storeDao: StoreDao
+
+    companion object {
+        const val DATABASE_NAME = "store_db"
+        fun getInstance(context: Context): StoreDb {
+            return Room.databaseBuilder(
+                context,
+                StoreDb::class.java,
+                DATABASE_NAME
+            ).build()
+        }
+    }
+
+
+}
