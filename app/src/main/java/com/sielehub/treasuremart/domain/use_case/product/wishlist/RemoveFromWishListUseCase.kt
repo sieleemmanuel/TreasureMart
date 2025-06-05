448450539eefab3b21f1bdb459a03fba6f4dfaef
Siele <sielekim.emmanuel@gmail.com>
2025-06-05 15:09:46 +0300

    Add Remove from WishList use case unit test

--- app/src/main/java/com/sielehub/treasuremart/domain/use_case/product/wishlist/RemoveFromWishListUseCase.kt
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/product/wishlist/RemoveFromWishListUseCase.kt
@@ -1,17 +1,17 @@
 package com.sielehub.treasuremart.domain.use_case.product.wishlist
 
 import com.sielehub.treasuremart.core.Resource
-import com.sielehub.treasuremart.data.repository.StoreRepositoryImpl
+import com.sielehub.treasuremart.domain.repository.WishRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class RemoveFromWishListUseCase(
-    private val storeRepositoryImpl: StoreRepositoryImpl
+    private val wishRepository: WishRepository
 ) {
-    operator fun invoke(productId: Int): Flow<Resource<Boolean>> = flow {
+    operator fun invoke(productId: Int): Flow<Resource<Unit>> = flow {
         try {
             emit(Resource.Loading())
-            emit(Resource.Success(storeRepositoryImpl.removeFromWishlist(productId)))
+            emit(Resource.Success(wishRepository.removeFromWishlist(productId)))
         } catch (e: Exception) {
             emit(Resource.Error(e.localizedMessage ?: "An unknown error occurred"))
         }
---
+++ app/src/test/java/com/sielehub/treasuremart/domain/use_case/product/wishlist/RemoveFromWishListUseCaseTest.kt
@@ -0,0 +1,30 @@
+package com.sielehub.treasuremart.domain.use_case.product.wishlist
+
+import com.google.common.truth.Truth.assertThat
+import com.sielehub.treasuremart.core.Resource
+import com.sielehub.treasuremart.domain.repository.WishRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class RemoveFromWishListUseCaseTest {
+    private lateinit var removeFromWishListUseCase: RemoveFromWishListUseCase
+    val wishRepository: WishRepository = mockk()
+
+    @Before
+    fun setUp() {
+        removeFromWishListUseCase = RemoveFromWishListUseCase(wishRepository)
+    }
+
+    @Test
+    fun `Successful removal from wishlist should return Success`() = runTest {
+        val productId = 1
+        coEvery { wishRepository.removeFromWishlist(productId) } returns Unit
+        val result = removeFromWishListUseCase(productId).toList()
+        assertThat(result[1]).isInstanceOf(Resource.Success::class.java)
+    }
+
+}
