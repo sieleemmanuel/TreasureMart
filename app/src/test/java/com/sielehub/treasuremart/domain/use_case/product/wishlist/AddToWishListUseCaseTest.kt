48a18533f3dc4cefe461c27012cc289ef5d020f5
Siele <sielekim.emmanuel@gmail.com>
2025-06-05 13:03:13 +0300

    Refactor: AddToWishListUseCaseTest and AddToWishListUseCase

--- app/src/test/java/com/sielehub/treasuremart/domain/use_case/product/wishlist/AddToWishListUseCaseTest.kt
+++ app/src/test/java/com/sielehub/treasuremart/domain/use_case/product/wishlist/AddToWishListUseCaseTest.kt
@@ -2,8 +2,8 @@ package com.sielehub.treasuremart.domain.use_case.product.wishlist
 
 import com.google.common.truth.Truth.assertThat
 import com.sielehub.treasuremart.core.Resource
-import com.sielehub.treasuremart.data.repository.StoreRepositoryImpl
 import com.sielehub.treasuremart.domain.model.WishProduct
+import com.sielehub.treasuremart.domain.repository.WishRepository
 import com.sielehub.treasuremart.fakeProducts
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -14,17 +14,17 @@ import org.junit.Test
 
 class AddToWishListUseCaseTest {
     private lateinit var addToWishListUseCase: AddToWishListUseCase
-    val storeRepositoryImpl: StoreRepositoryImpl = mockk()
+    val wishRepository: WishRepository = mockk()
 
     @Before
     fun setUp() {
-        addToWishListUseCase = AddToWishListUseCase(storeRepositoryImpl)
+        addToWishListUseCase = AddToWishListUseCase(wishRepository)
     }
 
     @Test
     fun `Successful addition to wishlist should return Success`() = runTest {
         val product = fakeProducts.map { WishProduct(it.id, it) }.first()
-        coEvery { storeRepositoryImpl.addToWishlist(product) } returns Unit
+        coEvery { wishRepository.addToWishlist(product) } returns Unit
         val result = addToWishListUseCase(product).toList()
         assertThat(result[1]).isInstanceOf(Resource.Success::class.java)
     }
