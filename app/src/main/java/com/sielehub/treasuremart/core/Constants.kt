85c2a7e91fbc1604d0c8ef2870d38606898100f7
Siele <sielekim.emmanuel@gmail.com>
2025-06-11 08:38:49 +0300

    Add dummy order and payment method variables

--- app/src/main/java/com/sielehub/treasuremart/core/Constants.kt
+++ app/src/main/java/com/sielehub/treasuremart/core/Constants.kt
@@ -1,9 +1,13 @@
 package com.sielehub.treasuremart.core
 
+import com.sielehub.treasuremart.domain.model.Address
 import com.sielehub.treasuremart.domain.model.Cart
 import com.sielehub.treasuremart.domain.model.CartProduct
+import com.sielehub.treasuremart.domain.model.Geolocation
+import com.sielehub.treasuremart.domain.model.Order
 import com.sielehub.treasuremart.domain.model.Product
 import com.sielehub.treasuremart.domain.model.Rating
+import java.time.LocalDate
 import java.util.Date
 
 class Constants {
@@ -34,6 +38,12 @@ class Constants {
         const val RETURNED = "Returned"
     }
 
+    object PaymentMethod {
+        const val VISA = "VISA"
+        const val GOOGLE_PAY = "Google Pay"
+        const val CASH_ON_DELIVERY = "Cash on Delivery"
+    }
+
     companion object {
         const val PARAM_PRODUCT_ID = "productId"
         const val PARAM_PRODUCT_CATEGORY = "productCategory"
@@ -49,15 +59,6 @@ class Constants {
                 rating = Rating(rate = 3.9, count = 120)
             ),
             Product(
-                id = 2,
-                title = "Mens Casual Premium Slim Fit T-Shirts ",
-                price = 22.3,
-                description = "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-                category = "men's clothing",
-                image = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-                rating = Rating(rate = 4.1, count = 259)
-            ),
-            Product(
                 id = 3,
                 title = "Mens Cotton Jacket",
                 price = 55.99,
@@ -65,60 +66,6 @@ class Constants {
                 category = "men's clothing",
                 image = "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
                 rating = Rating(rate = 4.7, count = 500)
-            ),
-            Product(
-                id = 4,
-                title = "Mens Casual Slim Fit",
-                price = 15.99,
-                description = "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-                category = "men's clothing",
-                image = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-                rating = Rating(rate = 2.1, count = 430)
-            ),
-            Product(
-                id = 5,
-                title = "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-                price = 695.0,
-                description = "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-                category = "jewelery",
-                image = "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-                rating = Rating(rate = 4.6, count = 400)
-            ),
-            Product(
-                id = 6,
-                title = "Solid Gold Petite Micropave ",
-                price = 168.0,
-                description = "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-                category = "jewelery",
-                image = "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-                rating = Rating(rate = 3.9, count = 70)
-            ),
-            Product(
-                id = 7,
-                title = "White Gold Plated Princess",
-                price = 9.99,
-                description = "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-                category = "jewelery",
-                image = "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
-                rating = Rating(rate = 3.0, count = 400)
-            ),
-            Product(
-                id = 8,
-                title = "Pierced Owl Rose Gold Plated Stainless Steel Double",
-                price = 10.99,
-                description = "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
-                category = "jewelery",
-                image = "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
-                rating = Rating(rate = 1.9, count = 100)
-            ),
-            Product(
-                id = 9,
-                title = "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-                price = 64.0,
-                description = "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-                category = "electronics",
-                image = "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-                rating = Rating(rate = 3.3, count = 203)
             ),
             Product(
                 id = 10,
@@ -137,15 +84,6 @@ class Constants {
                 category = "electronics",
                 image = "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
                 rating = Rating(rate = 4.8, count = 319)
-            ),
-            Product(
-                id = 12,
-                title = "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
-                price = 114.0,
-                description = "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
-                category = "electronics",
-                image = "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
-                rating = Rating(rate = 4.8, count = 400)
             ),
             Product(
                 id = 13,
@@ -180,6 +118,24 @@ class Constants {
             CartProduct(1, 1),
             CartProduct(8, 4),
             CartProduct(10, 1)
+        )
+
+        val order = Order(
+            orderId = 179090630039539L,
+            address = Address(
+                city = "Pretoria",
+                number = 123456789,
+                street = "Address 1",
+                geolocation = Geolocation("123.456", "789.012"),
+                zipcode = "12345",
+                shippingFee = 258.00,
+                isDefault = true
+            ),
+            orderItems = cartProducts().take(2),
+            orderTotal = 1235.00,
+            orderDate = LocalDate.now().toString(),
+            orderStatus = OrderStatus.SHIPPED,
+            paymentMethod = PaymentMethod.VISA
         )
 
         fun myCart() = Cart(Date().toString(), 1, cartProducts(), 1)
