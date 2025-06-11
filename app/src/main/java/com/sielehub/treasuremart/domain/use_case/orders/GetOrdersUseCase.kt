98068e2dd2d2e5e60909defd77174f3a20c9a5ff
Siele <sielekim.emmanuel@gmail.com>
2025-06-11 08:40:10 +0300

    Refactor: GetOrdersUseCase to emit Flow

--- app/src/main/java/com/sielehub/treasuremart/domain/use_case/orders/GetOrdersUseCase.kt
+++ app/src/main/java/com/sielehub/treasuremart/domain/use_case/orders/GetOrdersUseCase.kt
@@ -4,18 +4,18 @@ import com.sielehub.treasuremart.core.Resource
 import com.sielehub.treasuremart.domain.model.Order
 import com.sielehub.treasuremart.domain.repository.OrderRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 
 class GetOrdersUseCase(
     private val orderRepository: OrderRepository,
 ) {
 
-    operator fun invoke(): Flow<Resource<List<Order?>>> = flow {
+    operator fun invoke(): Flow<Resource<List<Order>>> = flow {
+        emit(Resource.Loading())
         try {
-            emit(Resource.Loading())
-            val orders = orderRepository.getOrders().first()
-            emit(Resource.Success(data = orders))
+            orderRepository.getOrders().collect {
+                emit(Resource.Success(data = it))
+            }
         } catch (e: Exception) {
             emit(Resource.Error(e.localizedMessage ?: "An unknown error occurred"))
         }
